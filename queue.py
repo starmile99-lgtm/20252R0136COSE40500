1fb9e322d915dd2d7abab9caa2b8e3dea9c31306
starmile99-lgtm <starmile99@korea.ac.kr>
2025-12-19 16:29:19 +0900

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,14 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        return self.items.pop(0)
+
+    def is_empty(self):
+        return len(self.items) == 0
